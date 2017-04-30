11e156feb11ccc25c55098d698d977a26af29360
Posttwo <post159510@gmail.com>
2017-04-30 15:17:14 +0100

    Fixed #13

--- database/migrations/2017_04_29_152818_add_verified_permission.php
+++ database/migrations/2017_04_29_152818_add_verified_permission.php
@@ -18,6 +18,46 @@ class AddVerifiedPermission extends Migration
         $p->name = 'user.verified';
         $p->description = 'Requires user to be verified on FunnyJunk';
         $p->save();
+
+        $p = new Permission();
+        $p->name = 'user.patreon';
+        $p->description = 'Requires user to be an FJ Patreon';
+        $p->save();
+
+        $p = new Permission();
+        $p->name = 'user.occreator';
+        $p->description = 'Requires user to have an OC Creator Badge';
+        $p->save();
+
+        $p = new Permission();
+        $p->name = 'user.level100';
+        $p->description = 'Requires user to have an account level of 100';
+        $p->save();
+
+        $p = new Permission();
+        $p->name = 'user.level200';
+        $p->description = 'Requires user to have an account level of 200';
+        $p->save();
+
+        $p = new Permission();
+        $p->name = 'user.level400';
+        $p->description = 'Requires user to have an account level of 400';
+        $p->save();
+
+        $p = new Permission();
+        $p->name = 'user.level10';
+        $p->description = 'Requires user to have an account level of 10';
+        $p->save();
+
+        $p = new Permission();
+        $p->name = 'admin.roles';
+        $p->description = 'Administrative';
+        $p->save();
+
+        $p = new Permission();
+        $p->name = 'admin.logs';
+        $p->description = 'Administrative';
+        $p->save();
     }
 
     /**
@@ -27,6 +67,6 @@ class AddVerifiedPermission extends Migration
      */
     public function down()
     {
-        Permission::where('name', 'user.verified')->firstOrFail()->delete();
+        Permission::all()->delete();
     }
 }
