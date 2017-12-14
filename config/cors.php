2701ea9ce1ba561fed1115439123eb0bedc949d9
Posttwo <post159510@gmail.com>
2017-12-14 20:07:05 +0000

    Fix Cors

--- config/cors.php
+++ config/cors.php
@@ -11,7 +11,7 @@ return [
      |
      */
     'supportsCredentials' => true,
-    'allowedOrigins' => ['*.' . env("APP_URI"), 'funnyjunk.com', '*'],
+    'allowedOrigins' => ['*.' . env("APP_URI"), 'funnyjunk.com'],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'],
     'exposedHeaders' => [],
