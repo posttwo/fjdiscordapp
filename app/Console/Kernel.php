5dca0fa006760d784070480ee4418c6622b64085
Posttwo <posttwo@users.noreply.github.com>
2018-04-15 20:23:41 +0100

    Change timings of notifcation per Jettoms Suggestion

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -78,15 +78,15 @@ class Kernel extends ConsoleKernel
             $r = $fj->getModInfo();
             
             //$r -> sfw nsfw links
-            if($r->sfw > 40)
+            if($r->sfw > 30)
             {
                 $slack = new \App\Slack;
                 $slack->target = 'mod-social';
-                $slack->username =   "Baby Jettom";
+                $slack->username =   "Jettoms Peen";
                 $slack->text     =   null;
-                $slack->avatar   =   'https://cdn.discordapp.com/avatars/156530362862927880/d8ef373931ce86125e5d5dd2a5e5e75a.jpg';
+                $slack->avatar   =   'https://i.imgur.com/6G1qaAT.png';
                 $slack->title    = '';
-                $slack->text     = ':warning: DING DONG TOO MUCH SHIT :warning: <@&427487027429244929>';
+                $slack->text     = ':warning: DING DONG YOU HAVE AIDS :warning: <@&427487027429244929>';
 
                 $slack->embedFields = [ 'SFW' => $r->sfw,
                                         'NSFW' => $r->nsfw,
