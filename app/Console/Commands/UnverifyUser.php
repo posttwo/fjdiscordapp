277d01a4bcf1bf4ab4b806bf8228366d994220f9
root <root@ispconfig.ss.mongla.net>
2018-05-17 13:05:56 +0100

    Add unverify for FJEdu

--- app/Console/Commands/UnverifyUser.php
+++ app/Console/Commands/UnverifyUser.php
@@ -44,18 +44,17 @@ class UnverifyUser extends Command
         $this->line("Nickname: " . $user->nickname);
         $this->line("FJ Username: " . $fjusername);
 
-        if(!$this->confirm("Are you sure you want to UNVERIFY this user?"))
+        if($this->confirm("Are you sure you want to UNVERIFY this user?"))
         {
-            $this->error("Unverification Command Cancelled");
-            return false;
-        }
-        logger("Administrative user has deverified user ", ["id" => $user->id]);
-        $user->fjuser()->delete();
-        $this->info("Deleted assosciated FJUser");
-        //syncPermissions
-        $user->syncPermissions([]);
-        $this->info("Removed all Permissions assosciated with user");
-
+            //$this->error("Unverification Command Cancelled");
+           // return false;
+            logger("Administrative user has deverified user ", ["id" => $user->id]);
+            $user->fjuser()->delete();
+            $this->info("Deleted assosciated FJUser");
+            //syncPermissions
+            $user->syncPermissions([]);
+            $this->info("Removed all Permissions assosciated with user");
+	}
         if($this->confirm("Would you like to revoke note tokens for this user?"))
         {
             $this->info("Trying to delete note token from " . $fjusername);
@@ -63,5 +62,43 @@ class UnverifyUser extends Command
             $this->info($json);
             $this->info("Deleted!");
         }
+	if($this->confirm("Would you like to suspend this users FJEdu access?"))
+	{
+		$this->error("Function not yet available");
+		$postdata = http_build_query(
+			array('criteria[0][key]' => 'lastname',
+			      'criteria[0][value]' => $fjusername,
+			      'criteria[1][key]' => 'auth',
+			      'criteria[1][value]' => 'oauth2')
+		);
+		$opts = array('http' =>
+    			array(
+        		 'method'  => 'POST',
+        		 'header'  => 'Content-type: application/x-www-form-urlencoded',
+        		 'content' => $postdata
+    			)
+		);
+		$context = stream_context_create($opts);
+		$results = file_get_contents('https://edu.fjme.me/webservice/rest/server.php?wstoken='.env("MOODLE_TOKEN").'&wsfunction=core_user_get_users&moodlewsrestformat=json', false, $context);
+		$results = json_decode($results, true);
+		$this->info('FJEdu User: ' . $results['users'][0]['fullname']);
+		if($this->confirm("Is this the correct user?"))
+		{
+			logger("Administrative user suspended FJEdu account for ". $results['users'][0]['fullname']);
+			$postdata = http_build_query(
+                        	array('users[0][id]' => $results['users'][0]['id'], 'users[0][suspended]' => 1)
+	                	);
+			$opts = array('http' =>
+                        	array(
+                         	 'method'  => 'POST',
+                         	 'header'  => 'Content-type: application/x-www-form-urlencoded',
+                        	 'content' => $postdata
+                       	 	)
+                	);
+			
+			$context = stream_context_create($opts);
+			$results = file_get_contents('https://edu.fjme.me/webservice/rest/server.php?wstoken='.env("MOODLE_TOKEN").'&wsfunction=core_user_update_users&moodlewsrestformat=json', false, $context);
+		}
+	}
     }
 }
