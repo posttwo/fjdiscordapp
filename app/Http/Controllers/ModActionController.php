c0d0fe5c7e192236e196849963958b92a7eb9e67
Posttwo <posttwo@users.noreply.github.com>
2019-04-07 15:26:08 +0100

    Update ModActionController.php

--- app/Http/Controllers/ModActionController.php
+++ app/Http/Controllers/ModActionController.php
@@ -301,7 +301,7 @@ class ModActionController extends Controller
             $slack->username = 'Executive Whip';
             $slack->avatar = 'https://i.imgur.com/876EUYE.png';
             $slack->title = "Ratings Too High";
-            $slack->text = 'Unresolved conflicts is at ' . $unratedContent . ' <@!192722532875370496> :gay_pride_flag: <@!191311168835420162> ';
+            $slack->text = 'Unresolved conflicts is at ' . $unratedContent . ' <@!364587871421595649> :gay_pride_flag: <@!191311168835420162> ';
             $slack->embedFields = [];
             $slack->footer = "Review: https://fjme.me/mods/ratings/nobody";
             $slack->color = "warning";
