92204ee83cfa53999e336d94627c8b30ae740b61
Posttwo <posttwo@users.noreply.github.com>
2019-09-18 13:37:47 +0100

    Update ModCase.php

--- app/ModCase.php
+++ app/ModCase.php
@@ -134,9 +134,12 @@ class ModCase extends Model
                 } else {
                     $this->addInternalAnnotation('resolveLink', "I have no clue what this retard is doing {$link}");
                 }
-            } catch (Exception $e) {
+            } 
+            
+           
+        } catch (Exception $e) {
                 $this->addInternalAnnotation('resolveLink', "EXCEPTION: {$link} $e");
-            }
+        }
 
         }
         return $this;
