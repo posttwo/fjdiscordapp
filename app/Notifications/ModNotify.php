c7bae8b2a0f06beb7d2dc65d7b42c4fe177d5d66
Posttwo <posttwo@users.noreply.github.com>
2019-09-18 14:18:44 +0100

    Update ModNotify.php

--- app/Notifications/ModNotify.php
+++ app/Notifications/ModNotify.php
@@ -7,7 +7,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\SlackMessage;
 
-class ModNotify extends Notification implements ShouldQueue
+class ModNotify extends Notification
 {
     use Queueable;
 
