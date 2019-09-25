8e62fc4ff79823f22f8a8ccd7a23ec5f742a91bc
Posttwo <post159510@gmail.com>
2019-09-26 00:54:58 +0100

    add reviewing flags

---
+++ database/migrations/2019_09_02_00001_add_mod_rating_needs_review.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNeedsReview extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('f_j_contents', function($table) {
+            $table->boolean('needsReview')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('f_j_contents', function($table) {
+            $table->dropColumn('needsReview');
+        });
+    }
+}
