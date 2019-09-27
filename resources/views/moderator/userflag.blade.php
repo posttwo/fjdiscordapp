85a6434927a98c4eaddc1611da669bd7393c45cb
Posttwo <post159510@gmail.com>
2019-09-27 21:36:46 +0100

    ^

--- resources/views/moderator/userflag.blade.php
+++ resources/views/moderator/userflag.blade.php
@@ -12,6 +12,9 @@
 </script>
 @endsection
 @section('content')
+<div class="alert alert-danger">
+        <strong>Warning!</strong> PLEASE HALT WHILE I TEST A THING THANKS
+    </div>
 <div class="row">
     <div class="col-md-12">
             <div class="panel panel-default">
@@ -31,9 +34,9 @@
                                     >
                                         <td>
                                             <a href="https://funnyjunk.com/find/{{strtolower($flag->type)}}/{{$flag->cid}}?redirect=1">{{$flag->type}} {{$flag->cid}}</a>
-                                            @if($flag->reason == 'harassment' && $flag->patrolled_by == null)
+                                            @if($flag->userflags->firstWhere('reason', 'harassment') != null && $flag->patrolled_by == null)
                                                 <div class="alert alert-warning">
-                                                    <strong>Warning!</strong> This is a hrassment flag, please check if the flagged user has a mod note regarding {{$flag->flagger_username}} or recent comment show weird behavious.
+                                                    <strong>Warning!</strong> This is a hrassment flag, please check if the flagged user has a mod note regarding {{$flag->userflags->firstWhere('reason', 'harassment')->flagger_username}} or recent comment show weird behavious.
                                                 </div>
                                             @endif
                                         </td>
