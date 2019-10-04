603cf83055fafe57d89963bfac538eab1d184e87
Posttwo <posttwo@users.noreply.github.com>
2019-10-04 16:50:29 +0100

    Ignore oAuth exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -20,6 +20,7 @@ class Handler extends ExceptionHandler
         \Illuminate\Database\Eloquent\ModelNotFoundException::class,
         \Illuminate\Session\TokenMismatchException::class,
         \Illuminate\Validation\ValidationException::class,
+        \League\OAuth2\Server\Exception\OAuthServerException::class,
     ];
 
     /**
